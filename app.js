2416f1a1a1dc07bc62b054012ab07799e36242ca
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-08-05 19:53:18 -0300

    Comentarios generales en app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ function agregarAmigo() {
     let input = document.getElementById("amigo");
     let entrada = input.value;
     
+    // Si el input esta vacío
     if (!entrada) {
         alert("Por favor, inserte un nombre.")
     } else {
+        // En otro caso
         amigos.push(entrada);
         input.value = "";
-
         actualizarListaAmigos();
     }
 }
@@ -17,8 +18,10 @@ function agregarAmigo() {
 function actualizarListaAmigos() {
     const lista = document.getElementById("listaAmigos")
 
+    // Limpiamos la lista para evitar posibles bugs
     lista.innerHTML = "";
 
+    // Agregamos los elementos a la lista de amigos
     for (let amigo of amigos){
         const item = document.createElement("li");
         item.textContent = amigo;
@@ -29,13 +32,14 @@ function actualizarListaAmigos() {
 function sortearAmigo(){
     const resultado = document.getElementById("resultado");
 
+    // En caso de que el array amigos este vacío
     if (amigos.length === 0) {
         resultado.innerHTML = "<li>No hay amigos para sortear.</li>";
         return;
     }
 
     const indiceAleatorio = Math.floor(Math.random() * amigos.length);
-
+    
     const amigoSorteado = amigos[indiceAleatorio];
 
     resultado.innerHTML = `<li>${amigoSorteado}</li>`
