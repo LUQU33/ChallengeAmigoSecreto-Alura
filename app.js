7a9aa89e1a6b84743c7e1e0890bc264152be0cf7
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-08-05 19:49:57 -0300

    Implementacion funcion para sortear los amigos

--- app.js
+++ app.js
@@ -7,9 +7,36 @@ function agregarAmigo() {
     if (!entrada) {
         alert("Por favor, inserte un nombre.")
     } else {
-        console.log(entrada);
         amigos.push(entrada);
         input.value = "";
+
+        actualizarListaAmigos();
+    }
+}
+
+function actualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos")
+
+    lista.innerHTML = "";
+
+    for (let amigo of amigos){
+        const item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    const resultado = document.getElementById("resultado");
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = "<li>No hay amigos para sortear.</li>";
+        return;
     }
 
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`
 }
